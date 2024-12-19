8cedc19cce6ee04014a4435c0c10b3f188ec1596
Gabriel Colon <gcolon@ucsd.edu>
2024-12-19 10:50:32 -0800

    Added space/water theme with gradient and particle background

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import Projects from './components/Projects';
 import About from './components/About';
 import Contact from './components/Contact';
 import './App.css';
+import Particles from 'react-tsparticles';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Hero />
-      <Projects />
-      <About />
-      <Contact />
+      <Particles
+        options={{
+          background: {
+            color: "#0b0c10",
+          },
+          particles: {
+            color: {
+              value: "#66fcf1",
+            },
+            links: {
+              color: "#66fcf1",
+              distance: 150,
+              enable: true,
+            },
+            move: {
+              enable: true,
+              speed: 3,
+            },
+            size: {
+              value: 3,
+            },
+          },
+        }}
+      />
+      {/* Other components go here */}
     </div>
   );
 }
