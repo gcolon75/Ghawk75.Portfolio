dd7f1f4a9d1daefe09c6021db963f0f5a7a60b9c
Gabriel Colon <gcolon@ucsd.edu>
2024-12-19 14:28:25 -0800

    Fixed background image and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import About from './components/About';
 import Contact from './components/Contact';
-import Particles from 'react-tsparticles';
+import './App.css';
 
 function App() {
-  console.log("App component rendering...");
-
   return (
-    <div>
-      <Particles
-  options={{
-    background: {
-      color: "#0b0c10",
-    },
-    particles: {
-      color: { value: "#66fcf1" },
-      links: { color: "#66fcf1", distance: 150, enable: true },
-      move: { enable: true, speed: 3 },
-      size: { value: 3 },
-    },
-    fullScreen: {
-      zIndex: -1, // Push the particles behind other content
-    },
-  }}
-/>
+    <div
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/background.png)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed',
+      }}
+    >
       <Header />
       <Hero />
       <Projects />
