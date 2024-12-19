d09dc1ebe8c12bcea25dec2bfbb805f534d5ebaa
Gabriel Colon <gcolon@ucsd.edu>
2024-12-19 15:46:41 -0800

    Added flowchart feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Flowchart from './components/Flowchart';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import About from './components/About';
@@ -17,8 +18,10 @@ function App() {
       }}
     >
       <Header />
+      
       <Hero />
       <Projects />
+      <Flowchart />
       <About />
       <Contact />
     </div>
