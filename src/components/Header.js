5be31f1da31d9ceea0f55895cc9d3493395eab51
Gabriel Colon <gcolon@ucsd.edu>
2025-01-14 22:33:36 -0800

    Centered navbar, updated Header.css & Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './Header.css';
 
 function Header() {
-  return (
-    <header className="header">
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="header">
+            {/* Left-Aligned Logo */}
+            <div className="logo">Ghawk75</div>
+
+            {/* Centered Navigation */}
+            <nav className="nav-links">
+                <a href="#hero">Home</a>
+                <a href="#projects">Projects</a>
+                <a href="#skill-tree">Skill Tree</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
