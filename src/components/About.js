352a7f5ac936743def55387396d724fc0d2e754e
Gabriel Colon <gcolon@ucsd.edu>
2025-01-21 20:27:50 -0800

    fixed resolution

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
         <div className="about-section">
             <h2>About Me</h2>
             <p>
-                Hi, I'm Gabriel Colon, a Cognitive Science major specializing in Human Interaction and Design, 
+                Hi, I'm Gabriel Colón, a Cognitive Science major specializing in Human Interaction and Design, 
                 with a minor in Computational Social Science from UC San Diego. I'm passionate about the intersection 
                 of technology, design, and human behavior, with skills spanning project management, coding 
                 (Java, Python, Data Structures), and user experience design.
