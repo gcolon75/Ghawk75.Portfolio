da14d99b6044ed509a27190506fcb669cffddcce
Gabriel Colon <gcolon@ucsd.edu>
2025-03-25 18:03:10 -0700

    Updated projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,14 +4,19 @@ import './Projects.css';
 function Projects() {
     const projects = [
         {
+            title: "axle: Mobile Mechanic App/Website Design",
+            description: "A complete redesign concept for Spotify's mobile app with a focus on user experience.",
+            link: "https://docs.google.com/presentation/d/14b8Es2WwV-2lSqeyjJYokhlfT65xDO2K/edit?usp=sharing&ouid=109196431552780940505&rtpof=true&sd=true"
+        },
+        {
+            title: "Goalee: Startup Project Pitch",
+            description: "A campus-based social app encouraging in-person interaction and events at UCSD.",
+            link: "https://docs.google.com/presentation/d/1-r9TBUI4x7WzPsGiljHeOuw9V274GHv3/edit?usp=sharing&ouid=109196431552780940505&rtpof=true&sd=true"
+        },
+        {
             title: "Volleyball Mobile",
             description: "A UX/UI design concept for a volleyball app aimed at La Jolla Shores.",
             link: "https://docs.google.com/presentation/d/1lJ-Y6p10M3ivPvvfhBiTRQJaIojinK-lfXC_VAtoZPk/edit?usp=sharing"
-        },
-        {
-            title: "Spotify Redesign",
-            description: "A complete redesign concept for Spotify's mobile app with a focus on user experience.",
-            link: "https://docs.google.com/document/d/1tqb9SHftXjbO5D9aDpiCTBFTdf0qve3nVeCdvgrYjcw/edit?usp=sharing"
         },
         {
             title: "TritonGo App",
